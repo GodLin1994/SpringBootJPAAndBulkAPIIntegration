8a1f3a892052c21cf1ddc635a83c3210a8944b84
LoveMan <zhang520godlin@163.com>
2018-11-14 17:47:50 +0800

    commit sfdc2db2

--- src/main/java/com/example/demo/hardcode/Hardcode.java
+++ src/main/java/com/example/demo/hardcode/Hardcode.java
@@ -2,6 +2,6 @@ package com.example.demo.hardcode;
 
 public class Hardcode {
     public static final String username="zhang520lovelin@163.com";
-    public static final String password="zhang1111RaaDtQGQmvNGh9NLzVgWU2VZ";
+    public static final String password="*****";
 
 }
